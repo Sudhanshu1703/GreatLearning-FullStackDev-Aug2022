ab558598d37a81c13f103d0044962654d4791864
Sudhanshu Singh <sudhansu375@rediffmail.com>
2022-10-21 02:05:00 +0530

    Refactored code
    Corrected string formatting for new line from /n to \n
    Added an outer loop to iterate as per Total no. of targets needs to be achieved.

--- Sudhanshu_FOP_LabAlgo2/Q1Transaction.java
+++ Sudhanshu_FOP_LabAlgo2/Q1Transaction.java
@@ -6,41 +6,42 @@ class Q1Transaction {
 	public static void main(String args[]) throws Exception {
 		
 		Scanner s = new Scanner(System.in);
-		System.out.println("enter the size of transaction array");
+		System.out.println("Enter the size of transaction array");
 		int size = s.nextInt();
 		int arr[] = new int[size];
 		int arrSum = 0;
 		int arrTempSum = 0;
 		
-		System.out.println("enter the values of array");
+		System.out.println("\nEnter the values of array");
 
 		for (int i=0; i<size; i++){
 			arr[i] = s.nextInt();
 			arrSum += arr[i];
 		}
 
-		System.out.println("Total value of Transactions : " + arrSum);
+		System.out.println("\nTotal value of Transactions : " + arrSum);
 
-		System.out.println("/nEnter the total no of targets that needs to be achieved");
+		System.out.println("\nEnter the total no of targets that needs to be achieved");
 		int targetNo = s.nextInt();
-		System.out.println("/nTotal No. of Target : " + targetNo);
+		System.out.println("\nTotal No. of Target : " + targetNo);
 
-		System.out.println("/nEnter the value of target");
-		int targetVal = s.nextInt();
-		System.out.println("/nTarget Value : " + targetVal);
+		for(int x=0; x<targetNo; x++){
+			System.out.println("\nEnter the value of target-"+(x+1));
+			int targetVal = s.nextInt();
+			System.out.println("\nTarget Value : " + targetVal);
 
-		if(targetVal > arrSum)
-		{
-			System.out.println("/nGiven target is not achieved : " + targetVal + " > " + arrSum);
-		}
+			if(targetVal > arrSum)
+			{
+				System.out.println("\nGiven target is not achieved : " + targetVal + " > " + arrSum);
+			}
 
+			for (int i=0; i<=size-1; i++){
+				arrTempSum += arr[i];
 
-		for (int i=0; i<=size-1; i++){
-			arrTempSum += arr[i];
-
-			if(arrTempSum >= targetVal) {
-					System.out.println("/nTarget achieved after " + (i+1) + " transaction(s)");
-					break;
+					if(arrTempSum >= targetVal) {
+						System.out.println("\nTarget achieved after " + (i+1) + " transaction(s)");
+						break;
+					}
 			}
 		}
 	}
